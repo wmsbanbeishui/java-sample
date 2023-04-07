cb5ee7721b7311c8766353a51e09211241f8900c
wangmengsi <269243737@qq.com>
2023-04-07 15:14:10 +0800

    空

---
+++ src/test/java/com/drhs/JavaSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drhs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
