cf4db13f9476f3b26c320d8920ced433ad66dfc7
wangmengsi <269243737@qq.com>
2023-04-11 11:04:23 +0800

    save

--- common/src/main/java/com/drhs/config/DruidConfig.java
+++ common/src/main/java/com/drhs/config/DruidConfig.java
@@ -23,7 +23,7 @@ public class DruidConfig {
        @ConfigurationProperties(prefix = "spring.datasource")：作用就是将 全局配置文件中
        前缀为 spring.datasource的属性值注入到 com.alibaba.druid.pool.DruidDataSource 的同名参数中
      */
-    @ConfigurationProperties(prefix = "spring.datasource.druid")
+    @ConfigurationProperties(prefix = "spring.datasource")
     @Bean
     public DataSource druidDataSource() {
         return new DruidDataSource();
